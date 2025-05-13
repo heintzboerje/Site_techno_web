3d9f8fd84ec68a0a9f75b5948d4f7c1fbd02af01
ZeinebouNiang <zeinang985@gmail.com>
2025-05-13 17:56:00 +0200

    Update datetime.js

--- SITE_Niang_Polo/Code/Javascript/datetime.js
+++ SITE_Niang_Polo/Code/Javascript/datetime.js
@@ -1,3 +1,7 @@
+
+//Zeinebou
+//Mets la date et heure a laquelle la page a été chargée
+
 window.addEventListener('load', function() {
     let now = new Date();
     document.getElementById('datetime').textContent = now.toLocaleString();
