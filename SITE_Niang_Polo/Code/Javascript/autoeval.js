5090400aaaf21081f7ac020906a1b56d0999af02
Voyager <heintzrboerje@protonmail.com>
2025-05-14 10:08:40 +0200

    autoeval.js 8b. J'ai toujours pas changé le calcule de la moyenne mais je le ferai

--- SITE_Niang_Polo/Code/Javascript/autoeval.js
+++ SITE_Niang_Polo/Code/Javascript/autoeval.js
@@ -1,4 +1,5 @@
-// Bentz
+// Bentz Polo
+
 // Calcule la moyenne
 function moyenne(list) {
   let result = 0;
@@ -8,15 +9,56 @@ function moyenne(list) {
   return result / 40;
 };
 
-// fonction principale
+// fonction qui met la moyenne dans l'élément input
 function getAverage() {
-  let input_list = document.querySelectorAll('input[type="range"]');
+  let inputs = document.querySelectorAll('input[type="range"]');
   let average = document.getElementById("average");
-  average.setAttribute("value", moyenne(input_list));
+  average.setAttribute("value", moyenne(inputs));
 };
 
+// fonction qui permet de lier getAverage à l'événement "input" des inputs
 function watchAverage() {
   for(let i = 0; i < document.querySelectorAll('input[type="range"]').length; i++){
     document.querySelectorAll('input[type="range"]')[i].addEventListener("input", getAverage);
   }
 }
+
+// Change le style selon la valeur de l'input range
+function changeLabelStyle(label, value){
+    if(value == 0){
+      label.style.color = "white";
+      label.style.background = "red";
+    } else if (value == 100) {
+      label.style.color = "white";
+      label.style.background = "green";
+    } else {
+      label.style.color = "white";
+      label.style.background = "orange";
+    }
+}
+
+// chargement et usage des fonctions après chargement de la page html.
+window.onload = function () {
+
+  // Array vide qui va contenir les labels
+  let labels = new Array();
+  // Array des inputs
+  let inputs = document.querySelectorAll('input[type="range"]');
+  // Pour chaque élément de inputs on récupère le label et on l'insère dans labels.
+  for(let i = 0; i < inputs.length; i++){
+    labels.push(document.querySelector('label[for="' + inputs[i].id + '"]'))
+  };
+
+  getAverage();
+  
+  watchAverage();
+  
+  for(let i = 0; i < inputs.length; i++){
+    // On appel la fonction une première fois pour appliquer le style initialement
+    changeLabelStyle(labels[i], inputs[i].value);
+    // Et on la lie à chaque input et leur labels respectif
+    inputs[i].addEventListener("input", function () {
+      changeLabelStyle(labels[i], inputs[i].value)
+    })
+  }
+};
